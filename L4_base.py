ce40e5ac7ef64a5fe439b32f74a7163ed19743ae
Luis Diego Pacheco <luis.dipa@hotmail.com>
2020-11-17 15:35:07 -0600

    codigo ya funciona con los nuevos parametros

--- L4_base.py
+++ L4_base.py
@@ -6,9 +6,14 @@ import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
-# Variables aleatorias A y Z
-vaA = stats.norm(3, np.sqrt(10))
-vaZ = stats.uniform(-np.pi/2, np.pi)
+# Parámetros
+w_0 = 4
+delta = 1
+
+# Variables aleatorias X y Y
+vaX = stats.norm(0, np.sqrt(delta*delta))
+vaY = stats.norm(0, np.sqrt(delta*delta))
+
 
 # Creación del vector de tiempo
 T = 100			# número de elementos
@@ -17,22 +22,22 @@ t = np.linspace(0, t_final, T)
 
 # Inicialización del proceso aleatorio X(t) con N realizaciones
 N = 10
-X_t = np.empty((N, len(t)))	# N funciones del tiempo x(t) con T puntos
+W_t = np.empty((N, len(t)))	# N funciones del tiempo x(t) con T puntos
 
 # Creación de las muestras del proceso x(t) (A y Z independientes)
 for i in range(N):
-	A = vaA.rvs()
-	Z = vaZ.rvs()
-	x_t = A * np.cos(np.pi*t + Z)
-	X_t[i,:] = x_t
-	plt.plot(t, x_t)
+	X = vaX.rvs()
+	Y = vaY.rvs()
+	w_t = X * np.cos(w_0*t) + Y * np.sin(w_0*t)
+	W_t[i,:] = w_t
+	plt.plot(t, w_t)
 
 # Promedio de las N realizaciones en cada instante (cada punto en t)
-P = [np.mean(X_t[:,i]) for i in range(len(t))]
+P = [np.mean(W_t[:,i]) for i in range(len(t))]
 plt.plot(t, P, lw=6)
 
 # Graficar el resultado teórico del valor esperado
-E = 6/np.pi * np.cos(np.pi*t)
+E = np.zeros(1) * t
 plt.plot(t, E, '-.', lw=4)
 
 # Mostrar las realizaciones, y su promedio calculado y teórico
@@ -54,16 +59,16 @@ plt.figure()
 # Cálculo de correlación para cada valor de tau
 for n in range(N):
 	for i, tau in enumerate(desplazamiento):
-		corr[n, i] = np.correlate(X_t[n,:], np.roll(X_t[n,:], tau))/T
+		corr[n, i] = np.correlate(W_t[n,:], np.roll(W_t[n,:], tau))/T
 	plt.plot(taus, corr[n,:])
 
 # Valor teórico de correlación
-Rxx = 19/2 * np.cos(np.pi*taus)
+Rww = 19/2 * np.cos(np.pi*taus)
 
 # Gráficas de correlación para cada realización y la
-plt.plot(taus, Rxx, '-.', lw=4, label='Correlación teórica')
+plt.plot(taus, Rww, '-.', lw=4, label='Correlación teórica')
 plt.title('Funciones de autocorrelación de las realizaciones del proceso')
 plt.xlabel(r'$\tau$')
-plt.ylabel(r'$R_{XX}(\tau)$')
+plt.ylabel(r'$R_{WW}(\tau)$')
 plt.legend()
 plt.show()
